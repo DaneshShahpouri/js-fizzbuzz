46a66e35df0347deddd8acbb1bde0bc4fb75f957
DaneshShahpouri <danesh.shahpouri@gmail.com>
2023-02-24 13:26:27 +0100

    Staffholding + console.log

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// - FOR (let i=1,100){
+//      if (i)
+//      console.log(i)
+//   }
+
+
+for(let i = 1; i<= 100; i++){
+    if( i % 3 == 0 && i % 5 == 0){
+        console.log("FizzBuzz");
+    }else if ( i % 3 == 0 ){
+        console.log("Fizz");
+    }else if ( i % 5 == 0 ){
+        console.log("Buzz");
+    }else{
+        console.log(i)
+    }
+}
